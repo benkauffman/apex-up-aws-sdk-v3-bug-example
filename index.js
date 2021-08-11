3c99c9f7fe33346fa91ed18fe71e296d345c681d
Ben Kauffman <ben@krashidbuilt.com>
2021-08-11 15:46:36 -0700

    adding broken example

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // this is a hack because apex up injects this environment variable and V3 AWS SDK doesn't like it
-delete process.env['AWS_PROFILE'];
+// delete process.env['AWS_PROFILE'];
 require('./src/server');
